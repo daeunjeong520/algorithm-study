47928c5f5c8bb3f99e186649646b9769c973b1a8
Daeun Jeong <jungde1345@gmail.com>
2023-05-16 18:09:54 +0900

    programmers level2 +1

---
+++ src/programmers/level2/NCommonMultiple.java
@@ -0,0 +1,30 @@
+package programmers.level2;
+
+public class NCommonMultiple {
+
+    public static int solution(int[] arr) {
+        int result = lcm(arr[0], arr[1]);
+
+        for(int i = 2; i < arr.length; i++) {
+            result = lcm(result, arr[i]);
+        }
+        return result;
+    }
+
+    public static int gcd(int a, int b) {
+        if(a % b == 0) return b;
+        else return gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        int result1 = solution(new int[]{2, 6, 8, 14});
+        System.out.println(result1);
+
+        int result2 = solution(new int[]{1, 2, 3});
+        System.out.println(result2);
+    }
+}
